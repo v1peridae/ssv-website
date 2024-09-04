6c63692c5d11555e1b5b3d4bad97901cca614d99
Louisa Mulei <louisakavindu@gmail.com>
2024-09-04 08:23:09 +0300

    changed html and css

--- script.js
+++ script.js
@@ -1,28 +1,27 @@
-var proceedButton = document.getElementById ("proceedButton");
+var proceedButton = document.getElementById("proceedButton");
 
-if (proceedButton){
+if (proceedButton) {
   proceedButton.addEventListener("click", redirectHome);
 }
 
 function redirectHome() {
-  location.href = ("home.html");
-  console.log = ("You're on the Home Page");
+  location.href = "home.html";
+  console.log = "You're on the Home Page";
 }
 
-
 var mythsList = [
-  "Myth - Sexual violence is as a result of clothing. <br> Fact - The only cause of sexual violence is choosing to violate another person. The victim’s appearance has no effect on their right to say no.",
-  
-  "Myth - Sexual violence only happens to women. <br> Fact - Men as well are victims of sexual violence.",
-  
-  "Myth - Only strangers are likely to sexually assault someone. <br> Fact - Among victims aged 18 to 29, ⅔ of them knew the offender. ",
+  "Myth - Sexual violence is as a result of clothing. <br><br> Fact - The only cause of sexual violence is choosing to violate another person. The victim’s appearance has no effect on their right to say no.",
+
+  "Myth - Sexual violence only happens to women. <br><br> Fact - Men as well are victims of sexual violence.",
+
+  "Myth - Only strangers are likely to sexually assault someone. <br><br> Fact - Among victims aged 18 to 29, ⅔ of them knew the offender. ",
 ];
 
 var fact = document.getElementById("fact");
-var generateFactsButton = document.getElementById ("generateFactsButton");
+var generateFactsButton = document.getElementById("generateFactsButton");
 var count = 0;
 
-if (generateFactsButton){
+if (generateFactsButton) {
   generateFactsButton.addEventListener("click", displayFact);
 }
 
@@ -31,7 +30,6 @@ function displayFact() {
   count++;
   if (count == mythsList.length) {
     count = 0;
-      console.log("mythsList fact:", mythsList[count]);
-
+    console.log("mythsList fact:", mythsList[count]);
   }
 }
